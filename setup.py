bb1f30e2384b2c98855b3dd968f63a4a1a65a00d
Britt <141253508+Bal67@users.noreply.github.com>
2024-08-07 11:36:42 -0500

    Update setup.py
    Creation of setup.py with necessary configuration to package application

--- setup.py
+++ setup.py
@@ -1,1 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="skin_condition_predictor",
+    version="0.1",
+    description="A Streamlit application to predict skin conditions using a fine-tuned MobileNetV2 model.",
+    author="Your Name",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "streamlit",
+        "tensorflow",
+        "pandas",
+        "numpy",
+        "boto3",
+        "Pillow",
+        "scikit-learn"
+    ],
+    entry_points={
+        'console_scripts': [
+            'skin_condition_predictor = skin_condition_predictor.app:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
 
